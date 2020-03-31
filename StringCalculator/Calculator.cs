a259e7b7d374ac00ff9cd980157467c6928855fb
Evgeniya Ryabokon <tweetty.jane@yandex.ru>
2020-03-31 12:40:57 +0500

    initial commit

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        private char[] availableOperations = new char[] { '-', '+' };
+
+        private struct NumbersAndOperations
+        {
+            public List<int> numbers;
+            public List<char> operations;
+
+            public NumbersAndOperations(List<int> numbers, List<char> operations)
+            {
+                this.numbers = numbers;
+                this.operations = operations;
+            }
+        }
+
+        private NumbersAndOperations Parse(string input)
+        {
+            var numbers = new List<int>();
+            var operations = new List<char>();
+
+            numbers = input.Split(availableOperations, StringSplitOptions.None).Select(Int32.Parse).ToList();
+
+            foreach (var item in input)
+            {
+                if (item == '+' || item == '-')
+                {
+                    operations.Add(item);
+                }
+            }
+
+            return new NumbersAndOperations(numbers, operations);
+        }
+
+        public int CalculateString(string input)
+        {
+            return Sum(Parse(input));
+        }
+
+        private int Sum(NumbersAndOperations splittedStrings)
+        {
+            var sum = splittedStrings.numbers[0];
+
+            for (int i = 1; i < splittedStrings.numbers.Count; i++)
+            {
+                if (splittedStrings.operations[i - 1] == '+')
+                {
+                    sum += splittedStrings.numbers[i];
+                }
+                if (splittedStrings.operations[i - 1] == '-')
+                {
+                    sum -= splittedStrings.numbers[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
+
---
+++ StringCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace StringCalculator.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(string input)
+        {
+            var calculator = new Calculator();
+            var result = calculator.CalculateString(input.Trim().Replace(" ", "+"));
+            return new string[] { result.ToString() };
+        }
+
+    }
+}
